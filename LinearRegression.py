0a22d05808a15e65d7bcc46a4369319ff9ba1ab1
Neeharica <neeharica.pallapati8@gmail.com>
2023-02-22 22:44:23 +0530

    Adding Linear Regression project

--- LinearRegression.py
+++ LinearRegression.py
@@ -12,7 +12,7 @@ price = houseData['price']
 
 # As in pandas .csv will read into dataframes but machine learning handle arrays not dataframes
 # So for this we were using numpy.
-# Here if we use size or price directly we will get indexes of the values so, to remove the index we were suing reshape
+# Here if we use size /price directly we will get indexes of the values so, to remove the index we were using reshape()
 # reshape (4,3) means 4 arrays each with 3 elements (columns)
 # i.e reshape(-1,1) means 1 column and -1 means python will figure out the value
 x = np.array(size).reshape(-1, 1)
@@ -25,12 +25,13 @@ model = LinearRegression()
 model.fit(x, y)
 
 # After training the model with the help of fit function we can evaluate MSE and R values
-regression_model_mse = mean_squared_error(x,y)
+regression_model_mse = mean_squared_error(x, y)
 print("MSE: ", math.sqrt(regression_model_mse))
-print("R squared value: ", model.score(x,y))
+print("R squared value: ", model.score(x, y))
 
 # we can get b values after the model fit
-# b0--> intercept between the linear regression model line and as far as the price access (y-intercept the value of y when x=0)
+# b0--> intercept between the linear regression model line and as far as the price access
+# (y-intercept the value of y when x=0)
 print(model.coef_[0])
 # b1 --> slope of the give linear regression (slope or how y changes per unit increase in x)
 print(model.intercept_[0])
@@ -45,4 +46,3 @@ plt.show()
 
 # Finally predicting prices based on size
 print("Prediction by the model: ", model.predict([[2000]]))
-
